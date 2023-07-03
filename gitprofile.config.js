4454933d644980bf7394ab78ec453ad8fb28282a
Nikhil Bhargava <56692685+nikhilxbhargava@users.noreply.github.com>
2023-07-03 15:05:59 -0700

    corrected username

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,9 +2,9 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'nikhilxbhargava', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
